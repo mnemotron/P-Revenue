3da772217effcf63bb82e2224ec9f0261e95c452
mnemotron <mnemotron@mnemotron>
2017-09-22 14:49:01 +0200

    Initial Commit.

---
+++ revenue.test/src/revenue/test/loc/bond/BondTest_CalcReturnTimeLine.java
@@ -0,0 +1,31 @@
+package revenue.test.loc.bond;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import revenue.core.bond.Bond;
+import revenue.entity.BondHeader;
+import revenue.entity.BondItemBuy;
+
+public class BondTest_CalcReturnTimeLine
+{
+
+	public static void main(String[] args)
+	{
+		// ITEMS
+		// nominal value: 2000, buy date: 01.02.2017, buy value: 100%
+		BondItemBuy locBondItem1 = new BondItemBuy(2000, new Date(117, 1, 1), 100, null, null);
+		
+		ArrayList<BondItemBuy> locBondItemsBuy = new ArrayList<BondItemBuy>();
+		locBondItemsBuy.add(locBondItem1);
+		
+		// HEADER
+		// interest date: 21.05., due date: 21.05.2020
+		BondHeader locBondHeader = new BondHeader("JUNG, DMS ANL.15/20", "Insurance", "A14J9D", "DE000A14J9D9", 6.0, BondHeader.INTEREST_INTERVALL_YEARLY, new Date(117, 4, 21), new Date(120, 4, 21), locBondItemsBuy);
+
+		Bond bond = new Bond(locBondHeader);
+
+		bond.calReturnTimeline();
+	}
+
+}
