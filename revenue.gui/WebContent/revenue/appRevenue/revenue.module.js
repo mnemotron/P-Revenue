fdc67b6264872e5aa63ee1fac400d3b0f4ffaf77
mnemotron <mnemotron@mnemotron>
2017-10-08 16:55:41 +0200

    Initial Commit.

--- revenue.gui/WebContent/revenue/appRevenue/revenue.module.js
+++ revenue.gui/WebContent/revenue/appRevenue/revenue.module.js
@@ -3,59 +3,64 @@
  */
 
 // MODULE
-var appRevenue = angular.module('appRevenue', [ 'ngRoute', 'ngSanitize', 'pascalprecht.translate' ]);
+var appRevenue = angular.module('appRevenue', ['ngRoute', 'ngSanitize', 'pascalprecht.translate']);
 
 // CONFIG
-appRevenue.config(function($routeProvider) {
+appRevenue.config(function($routeProvider){
+	
 	$routeProvider.when('/', {
-		templateUrl : 'viewPortfolioLaunchpad.htm',
-		controller : 'ctrlViewPortfolioLaunchpad'
+	templateUrl : 'viewPortfolioLaunchpad.htm',
+	controller : 'ctrlViewPortfolioLaunchpad'
 	}).when('/viewCreatePortfolio', {
-		templateUrl : 'viewCreatePortfolio.htm'
+		templateUrl : 'viewCreatePortfolio.htm',
+		controller : 'ctrlViewCreatePortfolio'
+	}).when('/viewPortfolio', {
+		templateUrl : 'viewPortfolio.htm'
 	});
 });
 
 appRevenue.config(function($translateProvider) {
-	
-	$translateProvider.useStaticFilesLoader({
-	    prefix: '../lang/lang-',
-	    suffix: '.json'
-	  });
 
-	  $translateProvider.preferredLanguage('de_DE');
-	  
-	  $translateProvider.useSanitizeValueStrategy('sanitize');
-	  
+	$translateProvider.useStaticFilesLoader({
+	prefix : '../lang/lang-',
+	suffix : '.json'
+	});
+
+	$translateProvider.preferredLanguage('de_DE');
+
+	$translateProvider.useSanitizeValueStrategy('sanitize');
+
 });
 
 // CONTROLLER
 appRevenue.controller('ctrlTranslate', function($scope, $translate) {
 
-	$scope.changeLang = function (key) {
-  $translate.use(key)
-//  .then(function (key) {
-//    console.log("Sprache zu " + key + " gewechselt.");
-//  }, function (key) {
-//    console.log("Irgendwas lief schief.");
-//  });
-};
+	$scope.changeLang = function(key) {
+		$translate.use(key)
+		// .then(function (key) {
+		// console.log("Sprache zu " + key + " gewechselt.");
+		// }, function (key) {
+		// console.log("Irgendwas lief schief.");
+		// });
+	};
 
 });
 
 appRevenue.controller('ctrlViewPortfolioLaunchpad', function($scope, $http) {
 
-	$http.get(
-			'http://localhost:8080/revenue.service/portfolio/getPortfolioList')
-			.then(function(response) {
-				$scope.portfolios = response.data
-			});
+			$http
+					.get('http://localhost:8080/revenue.service/portfolio/getPortfolioList')
+					.then(function(response) {
+						$scope.portfolios = response.data
+					});
 
-});
+		});
 
-appRevenue.controller('ctrlViewCreatePortfolio',function($scope, $http) {
+appRevenue.controller('ctrlViewCreatePortfolio', function($scope, $http) {
 
-					$scope.createPortfolio = function() {
-						$http.post('http://localhost:8080/revenue.service/portfolio/createPortfolio', $scope.portfolio);
-					};
+			$scope.createPortfolio = function() {
+				$http
+						.post('http://localhost:8080/revenue.service/portfolio/createPortfolio', $scope.portfolio);
+			};
 
-});
+		});
