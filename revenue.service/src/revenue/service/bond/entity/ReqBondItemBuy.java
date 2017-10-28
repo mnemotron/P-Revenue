66b605b4c64c0918ea4a18fdd8258301c3b37772
mnemotron <mnemotron@mnemotron>
2017-10-28 22:30:56 +0200

    Initial Commit.

---
+++ revenue.service/src/revenue/service/bond/entity/ReqBondItemBuy.java
@@ -0,0 +1,96 @@
+package revenue.service.bond.entity;
+
+import java.util.Date;
+
+public class ReqBondItemBuy
+{
+
+	private long id;
+
+	private long portfolioId;
+
+	private long depotId;
+
+	private long bondId;
+
+	private Date buyDate;
+
+	private double buyPercent;
+
+	private double nominalValue;
+
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public long getPortfolioId()
+	{
+		return portfolioId;
+	}
+
+	public void setPortfolioId(long portfolioId)
+	{
+		this.portfolioId = portfolioId;
+	}
+
+	public long getDepotId()
+	{
+		return depotId;
+	}
+
+	public void setDepotId(long depotId)
+	{
+		this.depotId = depotId;
+	}
+
+	public long getBondId()
+	{
+		return bondId;
+	}
+
+	public void setBondId(long bondId)
+	{
+		this.bondId = bondId;
+	}
+
+	public Date getBuyDate()
+	{
+		return buyDate;
+	}
+
+	public void setBuyDate(Date buyDate)
+	{
+		this.buyDate = buyDate;
+	}
+
+	public double getBuyPercent()
+	{
+		return buyPercent;
+	}
+
+	public void setBuyPercent(double buyPercent)
+	{
+		this.buyPercent = buyPercent;
+	}
+
+	public double getNominalValue()
+	{
+		return nominalValue;
+	}
+
+	public void setNominalValue(double nominalValue)
+	{
+		this.nominalValue = nominalValue;
+	}
+
+	public ReqBondItemBuy()
+	{
+
+	}
+}
