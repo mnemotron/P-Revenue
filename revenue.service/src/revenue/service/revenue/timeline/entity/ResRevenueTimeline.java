191073b57fc62b63c70fbd8970ffb88d9f969099
mnemotron <mnemotron@mnemotron>
2017-11-20 21:08:23 +0100

    Initial Timeline.

--- revenue.service/src/revenue/service/revenue/timeline/entity/ResRevenueTimeline.java
+++ revenue.service/src/revenue/service/revenue/timeline/entity/ResRevenueTimeline.java
@@ -2,35 +2,49 @@ package revenue.service.revenue.timeline.entity;
 
 import java.util.ArrayList;
 
-public class ResRevenueTimeline
-{
+public class ResRevenueTimeline {
 	private long portfolioId;
 
 	private ArrayList<ResDepot> depotList = new ArrayList<ResDepot>();
 
-	public ResRevenueTimeline()
-	{
+	private int startYear;
+
+	private int endYear;
+
+	public ResRevenueTimeline() {
 
 	}
 
-	public long getPortfolioId()
-	{
+	public long getPortfolioId() {
 		return portfolioId;
 	}
 
-	public void setPortfolioId(long portfolioId)
-	{
+	public void setPortfolioId(long portfolioId) {
 		this.portfolioId = portfolioId;
 	}
 
-	public ArrayList<ResDepot> getDepotList()
-	{
+	public ArrayList<ResDepot> getDepotList() {
 		return depotList;
 	}
 
-	public void setDepotList(ArrayList<ResDepot> depotList)
-	{
+	public void setDepotList(ArrayList<ResDepot> depotList) {
 		this.depotList = depotList;
+	}
+
+	public int getStartYear() {
+		return startYear;
+	}
+
+	public void setStartYear(int startYear) {
+		this.startYear = startYear;
+	}
+
+	public int getEndYear() {
+		return endYear;
+	}
+
+	public void setEndYear(int endYear) {
+		this.endYear = endYear;
 	}
 
 }
