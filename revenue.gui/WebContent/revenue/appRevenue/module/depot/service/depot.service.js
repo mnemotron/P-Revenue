1e8f8447a777456ebed7a535d52d24874ffad09a
mnemotron <mnemotron@mnemotron>
2017-12-20 13:46:23 +0100

    Service Core.

---
+++ revenue.gui/WebContent/revenue/appRevenue/module/depot/service/depot.service.js
@@ -0,0 +1,35 @@
+/**
+ * DEPOT.HTTP.SERVICE
+ */
+
+var depotService = angular.module('depot.service',
+    [
+    	'backend.service'
+	]);
+
+depotService.constant('URL_DEPOT_SERVICE', 
+			{	
+				SERVICE: '/depot/service', 
+				METHOD_GET_DEPOT_LIST: '/getDepotList',
+				METHOD_CREATE_DEPOT: '/createDepot'
+			});
+			
+																																																																																																				
+depotService.factory('depotService', function(backendService, URL_DEPOT_SERVICE) {
+	
+	function getDepotList(successCallback, errorCallback, config)
+	{	
+		backendService.httpGet(URL_DEPOT_SERVICE.SERVICE + URL_DEPOT_SERVICE.METHOD_GET_DEPOT_LIST, successCallback, errorCallback, config);
+	}
+	
+	function createDepot(successCallback, errorCallback, data)
+	{
+		backendService.httpPost(URL_DEPOT_SERVICE.SERVICE + URL_DEPOT_SERVICE.METHOD_CREATE_DEPOT, successCallback, errorCallback, data);
+	}
+	
+	return{
+		getDepotList: getDepotList,
+		createDepot: createDepot
+	};
+	
+});
