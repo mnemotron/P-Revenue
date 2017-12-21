b9f1415ec380248113e456e49e7b2663e127ef3a
mnemotron <mnemotron@mnemotron>
2017-12-21 14:52:24 +0100

    Service Core.

--- revenue.gui/WebContent/revenue/appRevenue/module/bond/bond.module.js
+++ revenue.gui/WebContent/revenue/appRevenue/module/bond/bond.module.js
@@ -30,39 +30,16 @@ bondModule.controller('ctrlViewBond', function($scope, $http, storageService, ST
 			$scope.$emit('notify', {type : 'E', msgId : 'viewBond.bond.bondItemBuyList.notify.error'});
 		}, {params : {portfolioId : $scope.selectedPortfolio.id, depotId : $scope.selectedDepot.id, bondId : $scope.selectedBond.id}});
 
-		// $http.get('http://localhost:8080/revenue.service/bond/service/getBondItemBuyList',
-		// {params : {portfolioId: $scope.selectedPortfolio.id, depotId:
-		// $scope.selectedDepot.id, bondId:
-		// $scope.selectedBond.id}}).then(function(response) {
-		// $scope.bondItemBuyList = response.data
-		//
-		// for (var i = 0; i < $scope.bondItemBuyList.length; i++) {
-		// $scope.bondItemBuyList[i].buyDate = new
-		// Date($scope.bondItemBuyList[i].buyDate);
-		// }
-		// });
-
 	}
 
 	$scope.deleteBond = function() {
 
 		bondService.deleteBond(function successCallback(response) {
-			location.path('/viewDepot');
+			$location.path('/viewDepot');
 		}, function errorCallback(response) {
 			logService.set('Revenue.Bond.Delete', LOGTYPE.ERROR, response.data);
 			$scope.$emit('notify', {type : 'E', msgId : 'viewBond.bond.delete.notify.error'});
 		}, {params : {bondId : $scope.selectedBond.id}});
-
-		// $http.delete('http://localhost:8080/revenue.service/bond/service/deleteBond',
-		// {params: {bondId : $scope.selectedBond.id}})
-		// .then(function successCallback(response) {
-		// $location.path( '/viewDepot' );
-		//			  
-		// },
-		//		
-		// function errorCallback(response) {
-		//			
-		// });
 	}
 
 	$scope.deleteBondItemBuy = function(index) {
@@ -75,23 +52,13 @@ bondModule.controller('ctrlViewBond', function($scope, $http, storageService, ST
 			logService.set('Revenue.Bond.BondItemBuy.Delete', LOGTYPE.ERROR, response.data);
 			$scope.$emit('notify', {type : 'E', msgId : 'viewBond.bondItemBuy.delete.notify.error'});
 		}, {params : {bondItemBuyId : bondItemBuyId}});
-
-		// $http.delete('http://localhost:8080/revenue.service/bond/service/deleteBondItemBuy',
-		// {params: {bondItemBuyId : bondItemBuyId}})
-		// .then(function successCallback(response) {
-		// $scope.getBond();
-		// },
-		//		
-		// function errorCallback(response) {
-		//			
-		// });
 	}
 
 	$scope.getBond();
 
 });
 
-bondModule.controller('ctrlViewAddBond', function($locale, $scope, $http, storageService, STORAGE_SERVICE_KEY, $location, BOND_LANGUAGE) {
+bondModule.controller('ctrlViewAddBond', function($locale, $scope, $http, storageService, bondService, logService, LOGTYPE, STORAGE_SERVICE_KEY, $location, BOND_LANGUAGE) {
 
 	// INIT
 	$scope.dateFormat = $locale.DATETIME_FORMATS.shortDate
@@ -119,25 +86,12 @@ bondModule.controller('ctrlViewAddBond', function($locale, $scope, $http, storag
 		}, function errorCallback(response) {
 			logService.set('Revenue.Bond.Create', LOGTYPE.ERROR, response.data);
 			$scope.$emit('notify', {type : 'E', msgId : 'viewAddBond.form.button.add.notify.error'});
-		}, 
-		$scope.bond
-	);
-
-		// $http.post('http://localhost:8080/revenue.service/bond/service/createBond',
-		// $scope.bond)
-		//
-		// .then(function successCallback(response) {
-		// $location.path('/viewDepot');
-		// },
-		//
-		// function errorCallback(response) {
-		//
-		// });
+		}, $scope.bond);
 	}
 
 });
 
-bondModule.controller('ctrlViewAddBondItemBuy', function($locale, $scope, $http, storageService, STORAGE_SERVICE_KEY, $location, BOND_LANGUAGE) {
+bondModule.controller('ctrlViewAddBondItemBuy', function($locale, $scope, $http, storageService, bondService, logService, LOGTYPE, STORAGE_SERVICE_KEY, $location, BOND_LANGUAGE) {
 
 	// INIT
 	$scope.dateFormat = $locale.DATETIME_FORMATS.shortDate
@@ -162,27 +116,12 @@ bondModule.controller('ctrlViewAddBondItemBuy', function($locale, $scope, $http,
 		$scope.bondItemBuy['depotId'] = depot.id;
 		$scope.bondItemBuy['bondId'] = bond.id;
 
-		bondService.createBondItemBuy(
-         function successCallback(response) {
-        	 $location.path('/viewBond');
-         }, 
-         function errorCallback(response) {
+		bondService.createBondItemBuy(function successCallback(response) {
+			$location.path('/viewBond');
+		}, function errorCallback(response) {
 			logService.set('Revenue.Bond.BondItemBuy.Create', LOGTYPE.ERROR, response.data);
 			$scope.$emit('notify', {type : 'E', msgId : 'viewAddBondItemBuy.form.button.buy.notify.error'});
-		}, 
-		$scope.bondItemBuy
-	);
-
-		// $http.post('http://localhost:8080/revenue.service/bond/service/createBondItemBuy',
-		// $scope.bondItemBuy)
-		//
-		// .then(function successCallback(response) {
-		// $location.path('/viewBond');
-		// },
-		//
-		// function errorCallback(response) {
-		//
-		// });
+		}, $scope.bondItemBuy);
 
 	}
 
